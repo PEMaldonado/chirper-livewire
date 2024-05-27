f38b1403fb570f5ecb0a9d8ffef5ec54d67939ad
PEMaldonado <pablomaldonado2086@gmail.com>
2024-05-27 11:48:30 -0300

    chirps-view-update

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
+Route::get('chirps', [ChirpController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('chirps');
+
 Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
+    ->middleware(['auth'])
     ->name('dashboard');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
